bc684617c37f00483d82471cc44d0f924310f5d0
Dimitry Zolotaryov <dimitry@webit.ca>
2017-09-03 17:03:41 -0700

    Add better loading indicator

--- NotesApp/NotesApp/RootViewController.swift
+++ NotesApp/NotesApp/RootViewController.swift
@@ -19,4 +19,29 @@ class RootViewController: UINavigationController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(loadingView)
+        
+        loadingView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        loadingView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        loadingView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        loadingView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    // MARK: - Public
+    
+    func showLoading(callback: (() -> Void)? = nil) {
+        loadingView.fadeIn(animated: true, callback: callback)
+    }
+    
+    func hideLoading(callback: (() -> Void)? = nil) {
+        loadingView.fadeOut(animated: true, callback: callback)
+    }
+    
+    // MARK: - Private
+    
+    private let loadingView = LoadingView()
 }
