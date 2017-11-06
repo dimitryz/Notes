46c3ef716e41bdad3393298ac83ed3be98102b88
Dimitry Zolotaryov <dimitry@webit.ca>
2017-11-05 21:28:02 -0500

    Fixed build script

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,24 +1,21 @@
+#
+# Configuration variables
+#
+
 DEPLOY_USER=deploy
-ADMIN_USER=admin
 SSH_HOST=127.0.0.1
-SSH_PORT=1234
-APP_HOME=/home/app
-DEPLOY_HOME=/home/deploy
-
-# Creates a new directory under: /var/app/DATE
-APP_DIR=`date  +%Y%m%dT%H%M%S`
-
-# The directory to which we'll deploy the app
-DEPLOY_ROOT="$DEPLOY_HOME/$APP_DIR"
+SSH_PORT=2222
 
 # Directory with the different code bases
-APP_SHARED_ROOT="$DEPLOY_ROOT/NotesShared"
-APP_SERVER_ROOT="$DEPLOY_ROOT/NotesServer"
-APP_EXEC_PATH="$APP_SERVER_ROOT/.build/release/NotesServer"
-APP_LINK_PATH="$APP_HOME/NotesServer"
+DEPLOY_HOME=/home/deploy
+DEPLOY_DIR=`date  +%Y%m%dT%H%M%S`
+DEPLOY_ROOT="$DEPLOY_HOME/$DEPLOY_DIR"
+DEPLOY_SHARED_ROOT="$DEPLOY_ROOT/NotesShared"
+DEPLOY_SERVER_ROOT="$DEPLOY_ROOT/NotesServer"
+DEPLOY_BUILD_PATH="$DEPLOY_SERVER_ROOT/.build"
 
-# Misc paths
-SUPERVISOR_CONF_PATH="/etc/supervisor/conf.d/NotesServer.conf"
+APP_HOME=/home/app
+APP_LINK_PATH="$APP_HOME/build"
 
 #
 # Starts the deploy process
@@ -39,7 +36,7 @@ rsync -av -e "ssh -p $SSH_PORT" NotesShared NotesServer \
 ssh -T $DEPLOY_USER@$SSH_HOST -p$SSH_PORT << EOSSH
 
 echo "-- Initializing Git in the shared code root"
-cd $APP_SHARED_ROOT
+cd $DEPLOY_SHARED_ROOT
 git config --global user.email "$DEPLOY_USER@localhost"
 git config --global user.name "deploy"
 git init
@@ -48,7 +45,7 @@ git commit -am "NotesShared"
 git tag 1.0.0
 
 echo "-- Building the server"
-cd $APP_SERVER_ROOT
+cd $DEPLOY_SERVER_ROOT
 sed -i 's/dependencies:\s*\[/dependencies: [\n    .Package(url: "\.\.\/NotesShared", majorVersion: 1),/g' Package.swift
 swift build -c release
 
@@ -56,22 +53,10 @@ echo "-- Stopping supervisor"
 supervisorctl stop all
 
 echo "-- Linking the app"
-ln -f -s $APP_EXEC_PATH $APP_LINK_PATH
-chown deploy:app $APP_LINK_PATH
-chown deploy:app $APP_EXEC_PATH
-chown 0750 $APP_EXEC_PATH
-
-echo "-- Configuring supervisor"
-cat << EOSF > $SUPERVISOR_CONF_PATH
-[program:NotesServer]
-autostart=true
-autorestart=true
-command=$APP_LINK_PATH
-directory=$APP_HOME
-user=app
-stdout_logfile=/var/log/supervisor/%(program_name)-stdout.log
-stderr_logfile=/var/log/supervisor/%(program_name)-stderr.log
-EOSF
+[ ! -f $APP_LINK_PATH ] || rm $APP_LINK_PATH
+ln -s $DEPLOY_BUILD_PATH $APP_LINK_PATH
+chown -R deploy:app $DEPLOY_BUILD_PATH
+chmod 0750 $DEPLOY_BUILD_PATH
 
 echo "-- Restarting supervisor"
 supervisorctl start all
